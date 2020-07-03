9da44392fbb39a2d81c9aefecd84cfb7ddce3423
nicoit <warp@nicoit.com.ar>
2020-07-03 00:57:37 -0300

    Initial commit

---
+++ app/Http/Controllers/ProxyController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Proxy\Proxy;
+use Proxy\Adapter\Guzzle\GuzzleAdapter;
+use Proxy\Filter\RemoveEncodingFilter;
+use Laminas\Diactoros\ServerRequestFactory;
+use GuzzleHttp;
+use Illuminate\Http\Request;
+
+class ProxyController extends Controller
+{
+    public function handle (Request $request) {
+        $client = new GuzzleHttp\Client([
+            // Base URI is used with relative requests
+            'base_uri' => 'https://docs.google.com',
+            // You can set any number of default request options.
+            'timeout'  => 2.0,
+        ]);
+        $response = $client->request('GET', $request->getRequestUri());
+        //dd($response->getBody(), $request);
+        return $response->getBody();
+    }
+
+    public function handle1 () {
+
+
+// Create a PSR7 request based on the current browser request.
+        $request = ServerRequestFactory::fromGlobals(['https://docs.google.com']);
+
+// Create a guzzle client
+        $guzzle = new GuzzleHttp\Client();
+
+// Create the proxy instance
+        $proxy = new Proxy(new GuzzleAdapter($guzzle));
+
+// Add a response filter that removes the encoding headers.
+        $proxy->filter(new RemoveEncodingFilter());
+
+// Forward the request and get the response.
+        //$request['headers']['host'] = 'https://docs.google.com';
+        //dd($request->getAttributes(), $request);
+        $response = $proxy->forward($request)->to('https://docs.google.com');
+
+// Output response to the browser.
+        (new \Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
+    }
+}
